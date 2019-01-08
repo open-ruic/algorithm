557050219103297559c9808f307fe55f96adc205
Chen Rui <chenrui1988@gmail.com>
2019-01-08 05:59:00 +0000

    Upgrade Leetcode Example 27,28,35,38,53

---
+++ src/main/java/me/arui/leetcode/MaximumSubarray53.java
@@ -0,0 +1,29 @@
+package me.arui.leetcode;
+
+/**
+ * 53. Maximum Subarray
+ * https://leetcode.com/problems/maximum-subarray/
+ */
+public class MaximumSubarray53 {
+
+    public int maxSubArray(int[] nums) {
+        int tempMax = nums[0];
+        int maxTotal = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            tempMax = Math.max(nums[i], nums[i] + tempMax);
+            if(tempMax > maxTotal) {
+                maxTotal = tempMax;
+            }
+        }
+        return maxTotal;
+    }
+
+    public static void test(MaximumSubarray53 test, int[] nums) {
+        System.out.println(test.maxSubArray(nums));
+    }
+
+    public static void main(String[] args) {
+        MaximumSubarray53 test = new MaximumSubarray53();
+        test(test, new int[] {-2,1,-3,4,-1,2,1,-5,4});
+    }
+}
---
+++ src/main/java/me/arui/leetcode/RemoveElement27.java
@@ -0,0 +1,47 @@
+package me.arui.leetcode;
+
+/**
+ * 27. Remove Element
+ * https://leetcode.com/problems/remove-element/
+ */
+public class RemoveElement27 {
+
+    public int removeElement(int[] nums, int val) {
+        int preIndex = 0;
+        int existNum = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == val) {
+                existNum++;
+            } else {
+                if(i != preIndex) {
+                    nums[preIndex] = nums[i];
+                }
+                preIndex = preIndex+1;
+            }
+        }
+        return nums.length - existNum;
+    }
+
+    public static void print(int length, int[] nums) {
+        for(int i = 0; i < length; i++) {
+            System.out.print(String.valueOf(nums[i]));
+            if(i != length -1) {
+                System.out.print("->");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void test(RemoveElement27 test, int[] nums, int val) {
+        int len = test.removeElement(nums, val);
+        print(len, nums);
+    }
+
+    public static void main(String[] args) {
+        RemoveElement27 test = new RemoveElement27();
+        test(test, new int[] {1,1,2}, 1);
+        test(test, new int[] {0, 1}, 1);
+        test(test, new int[] {1, 1}, 1);
+        test(test, new int[] {0,0,1,1,1,2,2,3,3,4}, 2);
+    }
+}
