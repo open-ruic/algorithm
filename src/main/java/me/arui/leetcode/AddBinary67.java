df450ded85f701b09f92a22e7e4cf951d3111b36
Chen Rui <chenrui1988@gmail.com>
2019-01-10 04:36:10 +0000

    Upgrade leecode 58 66 67 69 70

---
+++ src/main/java/me/arui/leetcode/AddBinary67.java
@@ -0,0 +1,45 @@
+package me.arui.leetcode;
+
+/**
+ * 67. Add Binary
+ * https://leetcode.com/problems/add-binary/
+ */
+public class AddBinary67 {
+
+    public String addBinary(String a, String b) {
+        if(a.length() < b.length()) return addBinary(b, a);
+        StringBuilder strBuilder = new StringBuilder(a);
+        boolean carry = false;
+        for(int i = 0; i < a.length(); i++) {
+            int sum = a.charAt(a.length() - i - 1) - 48;
+            if(i < b.length()) {
+                sum += b.charAt(b.length() - i - 1) - 48;
+            }
+            if(carry) {
+                sum++;
+            }
+            if(sum < 2) {
+                carry = false;
+                strBuilder.setCharAt(a.length() - i - 1, (char)(sum + 48));
+            } else if(sum >= 2) {
+                carry = true;
+                strBuilder.setCharAt(a.length() - i - 1, (char)(sum - 2 + 48));
+            }
+            if(i == a.length() -1 && carry) {
+                strBuilder.insert(0, '1');
+            }
+        }
+        return strBuilder.toString();
+    }
+
+    public static void test(AddBinary67 test, String a, String b) {
+        System.out.println(test.addBinary(a, b));
+    }
+
+    public static void main(String[] args) {
+        AddBinary67 test = new AddBinary67();
+        test.test(test, "11", "1");
+        test.test(test, "1010", "1011");
+    }
+
+}
---
+++ src/main/java/me/arui/leetcode/Sqrtx69.java
@@ -0,0 +1,34 @@
+package me.arui.leetcode;
+
+/**
+ * 69. Sqrt(x)
+ * https://leetcode.com/problems/sqrtx/
+ *
+ */
+public class Sqrtx69 {
+
+    public int mySqrt(int x) {
+        double precision = 0.0001f;
+        double tmp = x;
+        double result = x/(double)2;
+        while (tmp - result > precision) {
+            tmp = result;
+            result = (tmp + x/tmp)/(double)2;
+        }
+        return (int) Math.floor(result);
+
+    }
+
+    public static void test(Sqrtx69 test, int x) {
+        System.out.println(test.mySqrt(x));
+    }
+
+    public static void main(String[] args) {
+        Sqrtx69 test = new Sqrtx69();
+        test.test(test, 2147395599);
+        System.out.println(Math.sqrt(2147395599));
+//        test.test(test, 6);
+//        test.test(test, 3);
+//        test.test(test, 1);
+    }
+}
